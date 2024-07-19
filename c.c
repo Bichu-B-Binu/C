0c19f98dc5b91131f95e1ad18b3143f8b6c328e4
Bichu-B-Binu <bichubbinu77@gmail.com>
2024-07-19 17:22:45 +0530

    Sum of a number

--- c.c
+++ c.c
@@ -1,10 +1,23 @@
-// Online C compiler to run C program online
+/* simple program*/
+// #include <stdio.h>
+
+// int main() {
+//     // Write C code here
+//     int a;
+//     scanf("%d",&a);
+//     printf("you have entered %d",a);
+//     return 0;
+// }
+/*sum of a number*/
+
 #include <stdio.h>
 
-int main() {
+int main(void) {
     // Write C code here
-    int a;
-    scanf("%d",&a);
-    printf("you have entered %d",a);
+ int num1,num2,sum;
+ printf("Enter two number:");
+ scanf("%d%d",&num1,&num2);
+ sum=num1+num2;
+ printf("Result:%d",sum);
     return 0;
 }
