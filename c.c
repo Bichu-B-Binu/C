b87712c0be3e85bc3212a902cb3e723da9d9bc8a
Bichu-B-Binu <bichubbinu77@gmail.com>
2024-07-22 17:27:02 +0530

    nested if in C

--- c.c
+++ c.c
@@ -128,3 +128,30 @@
 //    }
 //     return 0;
 // }
+
+
+#include <stdio.h>
+
+int main() {
+   int num1,num2,num3;
+   printf("Enter first number:");
+   scanf("%d",&num1);
+   printf("Enter second number:");
+   scanf("%d",&num2);
+   printf("Enter third number:");
+   scanf("%d",&num3);
+   if(num1<num2){
+       if(num2<num3){
+             printf("The gratest number is %d",num3);
+       }else{
+             printf("The gratest number is %d",num2);
+       }
+   }else{
+       if(num1<num3){
+             printf("The gratest number is %d",num3);
+       }else{
+             printf("The gratest number is %d",num1);
+       }
+   }
+    return 0;
+}
