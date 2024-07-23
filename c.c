2cac90237b689427fac0ea1991b0b3327d5a3717
Bichu-B-Binu <bichubbinu77@gmail.com>
2024-07-23 16:07:36 +0530

    Switch case in days

--- c.c
+++ c.c
@@ -157,7 +157,7 @@
 // }
 
 
-#include <stdio.h>
+// #include <stdio.h>
 
 // int main() {
 //    int num1,num2,choice,result;
@@ -236,27 +236,68 @@
 // Write a program to show the grade obtained by a student after he/she enters their total mark percentage.
 
 
+// #include <stdio.h>
+
+// int main() {
+//     float totalMark;
+//     printf("Enter the student mark:");
+//     scanf("%f",&totalMark);
+//     if(totalMark>=90){
+//         printf("A Grade\n");
+//     }else if(totalMark>=80){
+//         printf("B Grade\n");
+//     }else if(70<=totalMark){
+//         printf("C Grade\n");
+//     }else if(60<=totalMark){
+//         printf("D Grade\n");
+//     }else if(50<=totalMark){
+//         printf("E Grade\n");
+//     }else if(totalMark<=50){
+//         printf("Failed");
+//     }else{
+//         printf("Your entered wroung data");
+//     }
+    
+//     return 0;
+// }
+
+
+
+
+// Using the ‘switch case’ write a program to accept an input number from the user and output the day as follows. 
+
+
 #include <stdio.h>
 
 int main() {
-    float totalMark;
-    printf("Enter the student mark:");
-    scanf("%f",&totalMark);
-    if(totalMark>=90){
-        printf("A Grade\n");
-    }else if(totalMark>=80){
-        printf("B Grade\n");
-    }else if(70<=totalMark){
-        printf("C Grade\n");
-    }else if(60<=totalMark){
-        printf("D Grade\n");
-    }else if(50<=totalMark){
-        printf("E Grade\n");
-    }else if(totalMark<=50){
-        printf("Failed");
-    }else{
-        printf("Your entered wroung data");
-    }
-    
+  int day;
+  printf("Pick one 1-7 days:");
+  scanf("%d",&day);
+  
+  switch(day){
+      case 1:
+      printf("Sunday");
+      break;
+      case 2:
+      printf("Monday");
+      break;
+      case 3:
+      printf("Tuesday");
+      break;
+      case 4:
+      printf("Wednesday");
+      break;
+      case 5:
+      printf("Thursday");
+      break;
+      case 6:
+      printf("Friday");
+      break;
+      case 7:
+      printf("Saturday");
+      break;
+      default:
+      printf("Invalid Entry");
+  }
     return 0;
 }
