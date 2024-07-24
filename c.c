5809d84de864df115f04de5a501a2f8ec246bc8a
Bichu-B-Binu <bichubbinu77@gmail.com>
2024-07-24 12:21:50 +0530

    Loop example

--- c.c
+++ c.c
@@ -267,37 +267,49 @@
 // Using the ‘switch case’ write a program to accept an input number from the user and output the day as follows. 
 
 
+// #include <stdio.h>
+
+// int main() {
+//   int day;
+//   printf("Pick one 1-7 days:");
+//   scanf("%d",&day);
+  
+//   switch(day){
+//       case 1:
+//       printf("Sunday");
+//       break;
+//       case 2:
+//       printf("Monday");
+//       break;
+//       case 3:
+//       printf("Tuesday");
+//       break;
+//       case 4:
+//       printf("Wednesday");
+//       break;
+//       case 5:
+//       printf("Thursday");
+//       break;
+//       case 6:
+//       printf("Friday");
+//       break;
+//       case 7:
+//       printf("Saturday");
+//       break;
+//       default:
+//       printf("Invalid Entry");
+//   }
+//     return 0;
+// }
+
+
 #include <stdio.h>
 
 int main() {
-  int day;
-  printf("Pick one 1-7 days:");
-  scanf("%d",&day);
-  
-  switch(day){
-      case 1:
-      printf("Sunday");
-      break;
-      case 2:
-      printf("Monday");
-      break;
-      case 3:
-      printf("Tuesday");
-      break;
-      case 4:
-      printf("Wednesday");
-      break;
-      case 5:
-      printf("Thursday");
-      break;
-      case 6:
-      printf("Friday");
-      break;
-      case 7:
-      printf("Saturday");
-      break;
-      default:
-      printf("Invalid Entry");
-  }
+   int i;
+   for(i=1;i<=100;i++){
+       printf("%d\n",i);
+   }
+
     return 0;
 }
