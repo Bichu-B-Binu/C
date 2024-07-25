1bde147e97a47ed86300703538e2d800839331d1
Bichu-B-Binu <bichubbinu77@gmail.com>
2024-07-25 11:12:07 +0530

    Multiplications of a number

--- c.c
+++ c.c
@@ -371,18 +371,50 @@
 // }
 
 
+// #include <stdio.h>
+
+// int main() {
+//   int i,num,j;
+//   printf("How many stars you want in reverse order:");
+//   scanf("%d",&num);
+//   for(i=1;i<=num;i++){
+//       for(j=num;j>=i;j--){
+//           printf("* ");
+//       }
+//       printf("\n");
+//   }
+
+//     return 0;
+// }
+
+
+// Write a program to print the multiplication table of given number
+// Accept an input from the user and display its multiplication table
+// Eg: 
+// Output: Enter a number
+// Input: 5
+// Output: 
+// 1 x 5 = 5
+// 2 x 5 = 10
+// 3 x 5 = 15
+// 4 x 5 = 20
+// 5 x 5 = 25
+// 6 x 5 = 30
+// 7 x 5 = 35
+// 8 x 5 = 40
+// 9 x 5 = 45
+// 10 x 5 = 50
+
 #include <stdio.h>
 
 int main() {
-  int i,num,j;
-  printf("How many stars you want in reverse order:");
+  int i,num;
+  printf("Enter your multiplication number:");
   scanf("%d",&num);
-  for(i=1;i<=num;i++){
-      for(j=num;j>=i;j--){
-          printf("* ");
-      }
-      printf("\n");
+  for(i=1;i<=10;i++){
+   
+     printf("%d*%d=%d\n",i,num,i*num);
   }
-
+  
     return 0;
 }
