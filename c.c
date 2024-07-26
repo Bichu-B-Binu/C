218211822bad1c5209a8622ec7f4be59b28ca9d3
Bichu-B-Binu <bichubbinu77@gmail.com>
2024-07-26 17:32:12 +0530

    q

--- c.c
+++ c.c
@@ -593,6 +593,18 @@
     
 //     return 0;
 // }
+// Write a program to interchange the values of two arrays
+// Program should accept an array from the user, swap the values of two arrays and display it on the console
+// Eg: Output: Enter the size of arrays
+// Input: 5
+// Output: Enter the values of Array 1
+// Input: 10, 20, 30, 40, 50
+// Output: Enter the values of Array 2
+// Input: 15, 25, 35, 45, 55
+// Output: Arrays after swapping:
+// Array1: 15, 25, 35, 45, 55
+// Array2: 10, 20, 30, 40, 50
+
 
 #include <stdio.h>
 
